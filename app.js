53dc5db1edf023205d8ccc0a7f61bdbbefc5210e
talhakhan211 <talhakhan9429@gmail.com>
2024-12-02 03:47:28 -0700

    calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
